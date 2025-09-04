da683623e36de033c12482aba17b6ab1cf3f2e01
Akihiro Nishiyama <aki16gaf@outlook.jp>
2025-09-04 22:22:34 +0900

    mod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
+
+import eslintPluginJs from "@eslint/js";
+
+export default [
+  eslintPluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off", // 未使用変数のチェックを無効化
+    },
+  },
 ];
-
-export default eslintConfig;
