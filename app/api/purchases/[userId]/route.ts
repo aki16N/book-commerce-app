b70ecb44ffbdb2dc14ab83968ff50d36a8ad98e3
Akihiro Nishiyama <aki16gaf@outlook.jp>
2025-09-07 21:17:00 +0900

    mod

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -2,8 +2,8 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 // 購入履歴検索API
-export async function GET(request: Request, { params }: { params: { uesrId: string } }) {
-    const userId = params.uesrId;
+export async function GET(request: Request, { params }: { params: { userId: string } }) {
+    const userId = params.userId;
 
     try {
         const purchases = await prisma.purchase.findMany({
